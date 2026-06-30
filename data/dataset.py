316ec2fec482ff262423ddb6c560083077117778
Sanjeet2835 <sanjeetsingh9717@gmail.com>
2026-06-30 17:57:36 +0000

    Remove obsolete root configs and data

--- data/dataset.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data import Dataset, DataLoader
-import os
-
-class PatchDataset:
-    
-    def __init__(self, root, split_path):
-        self.root = root
-        self.split_path = split_path 
-        self.input_tir = os.path.join(root, split_path)
